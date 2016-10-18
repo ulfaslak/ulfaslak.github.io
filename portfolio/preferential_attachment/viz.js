747ee31f10f1ed3fff6b34c952504adaa38bb8f7
ulfaslak <ulfjensen@gmail.com>
2016-10-18 18:04:19 +0200

    .

--- portfolio/preferential_attachment/viz.js
+++ portfolio/preferential_attachment/viz.js
@@ -1,190 +1,197 @@
-w = 510;
+w = 560;
 h = 510;
-s = 50;
-r = 15;
+r = 10;
 
-di = [
-    [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0], [10, 0],
-    [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7], [0, 8], [0, 9], [0, 10]
-]
-dg = ['zero','one','two','three','four','five','six','seven','eight','nine']
+sorted = false
+
+D = [0, 1]
+N = {0: [1], 1: [0]}
 
 w1 = document.getElementById("w1");
 w2 = document.getElementById("w2");
 
-n_nodes = 10;
-n_edges = n_nodes * (n_nodes - 1) / 2;
+var vis = d3.select("#w1").append("svg")
+        .attr("width", w)
+        .attr("height", h)
 
-var get_label = function(i, j) {
-    return [dg[i], dg[j]].sort().join("")
+var hist = d3.select("#w2")
+        .append("svg")
+        .attr("width", w+50)
+        .attr("height", h);
+
+// Controls
+function drawControls() {
+    vis.selectAll("controlslabel")
+        .data([0]).enter().append("svg:text")
+        .text("Controls")
+        .attr("x", 40 )
+        .attr("y", h - 20)
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "20px")
+        .attr("font-weight", "bold")
+        .attr("fill", "#2c3e50");
+
+    vis.selectAll("controls")
+        .data([["+1", 0.7], ["+10", 5],["r", 10.8], ["s", 15.5]])
+        .enter()
+        .append("svg:text")
+        .text(function(d) { return d[0]; })
+        .attr("x", function(d) { return 12.5*d[1] + 170; })
+        .attr("y", h - 20)
+        .attr("font-family", "sans-serif")
+        .attr("font-size", function(d) {
+            if (d[0] == "+1") { return "20px"; }
+            if (d[0] == "+10") {return "20px"; }
+            else { return "25px"; }
+        })
+        .attr("font-weight", "bold")
+        .attr("fill", "black");
+
+    vis.selectAll("addnodecircle")
+        .data([1, 4, 7, 10])
+        .enter()
+        .append("svg:circle")
+        .attr("cx", function(d) { return 20*d + 170; })
+        .attr("cy", h - 26)
+        .attr("r", 20)
+        .attr("fill", "#95a5a6")
+        .attr("opacity", 0.5)
+        .on("click", function(d) {
+            d3.select(this)
+                .transition()
+                .duration(100)
+                .style({'opacity': 0.8})
+                .transition()
+                .duration(100)
+                .style({'opacity': 0.5})
+            if (d == 1) {  
+                addRandomNode()
+            }
+            if (d == 4) {
+                playProcess()
+            }
+            if (d == 7) {
+                restart()
+            }
+            if (d == 10) {
+                sorted = !sorted;
+                clear_bars();
+                update_histogram();
+            }
+        
+        })
+        .on("mouseover", function(d) {
+            d3.select(this)
+                .transition()
+                .duration(100)
+                .style({'opacity': 0.8})
+        })
+        .on("mouseout", function(d) {
+            d3.select(this)
+                .transition()
+                .duration(100)
+                .style({'opacity': 0.5})
+        })
 }
 
-active = {}
-for (i=0; i<n_nodes; i++) {
-    for (j=0; j<n_nodes; j++) {
-        if (j <= i) { continue; }
-        active[get_label(i, j)] = false
+// Histogram
+function drawHist() {
+
+    window.clear_bars = function() {
+        hist.selectAll("rect")
+            .remove()
     }
-}
 
-// ---------------- //
-// Adjacency matrix //
-// ---------------- //
+    hist.selectAll("distlabel")
+        .data([0]).enter().append("svg:text")
+        .text("Degree distribution")
+        .attr("x", 190 )
+        .attr("y", 20)
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "20px")
+        .attr("font-weight", "bold")
+        .attr("fill", "#2c3e50");
 
-var svg_w1 = d3.select("#w1")
-    .append("svg")
-    .attr("width", w+50)
-    .attr("height", h+50);
+    window.update_histogram = function() {
+        N_hist = d3.keys(N)
+            .map(function(e, i) { return [e, N[i].length]; })
+            .sort(function(a, b) {
+                var x=a[1];
+                var y=b[1];
+                return y-x;
+            });
 
-svg_w1.selectAll("circle")
-    .data(di)
-    .enter()
-    .append("svg:circle")
-    .attr("cx", function(d) {
-        return d[0] * 51 + 29;
-    })
-    .attr("cy", function(d) {
-        return d[1] * 51 + 25;
-    })
-    .attr("r", r)
-    .attr("fill", "#95a5a6")
+        N_hist_max = Math.max(...N_hist.map(function(e) { return e[1]; }))
 
-svg_w1.selectAll("text")
-    .data(di)
-    .enter()
-    .append("text")
-    .text(function(d) { return Math.max(d[0], d[1])-1; })
-    .attr("x", function(d) {
-        return d[0] * 51 + 24.5;
-    })
-    .attr("y", function(d) {
-        return d[1] * 51 + 30;
-    })
-    .attr("font-family", "sans-serif")
-    .attr("font-size", "16px")
-    .attr("font-weight", "bold")
-    .attr("fill", "white");
+        xscale = d3.scale.linear()
+            .domain([0, N_hist.length])
+            .range([0, w]);
+        yscale = d3.scale.linear()
+            .domain([0, N_hist_max])
+            .range([30, h])
 
-svg_w1.selectAll(".background")
-    .data([0])
-    .enter()
-    .append("rect")
-    .attr("x", 49)
-    .attr("y", 49)
-    .attr("width", w+1)
-    .attr("height", h+1);
+        clear_bars()
 
-for (i=0; i<n_nodes; i++) {
-    for (j=0; j<n_nodes; j++) {
-        if (j > i) { 
-            data = [[i, j], [j, i]];
-        } else if (j == i) {
-            data = [[i, j]];
-        } else {
-            continue;
-        }
-        label = get_label(i, j)
-
-        var edgebox = svg_w1.selectAll(label)
-            .data(data)
+        hist.selectAll("bar")
+            .data(N_hist)
             .enter()
-
-        edgebox.append("rect")
-            .attr("class", label)
-            .attr("x", function(d) {
-              return d[0] * s + d[0] + 50;
-            })
-            .attr("y", function(d) {
-              return d[1] * s + d[1] + 50;
-            })
-            .attr("width", s)
-            .attr("height", s)
-            .attr("fill", "#ecf0f1")
-            .on("click", function(d) {
-                if (d[0] != d[1]) {
-                    label = get_label(d[0], d[1])
-                    if (active[label]) { 
-                        opacity = 1;
-                        active[label] = false
-                        graph.removeLink(d[0], d[1]);
-                        graph.removeLink(d[1], d[0]);
-                        keepNodesOnTop();
-                    }
-                    else if (!active[label]) {
-                        opacity = 0.7;
-                        active[label] = true
-                        graph.addLink(d[0], d[1], '20');
-                        keepNodesOnTop();
-                    }
-                    d3.selectAll("." + label)
-                        .transition()
-                        .duration(200)
-                        .style({'opacity': opacity});
-                } else {
-                    d3.select(this)
-                        .transition()
-                        .duration(100)
-                        .style({'opacity': 0.7})
-                        .transition()
-                        .duration(100)
-                        .style({'opacity': 1.0})
-                        
-                }
-            })
+            .append("rect")
+            .attr("x", function(d, i) { if (sorted) { return xscale(i); } else { return xscale(d[0]); }; })
+            .attr("y", function(d) { return h-yscale(d[1]) + 30 })
+            .attr("width", function(d) { return w/N_hist.length; })
+            .attr("height", function(d) { return yscale(d[1]) - 40; })
+            .attr("stroke-width", function(d) { return 10/Math.sqrt(d3.keys(N_hist).length)})       
     }
+
+    update_histogram()
 }
 
 
-// ----- //
-// Graph //
-// ----- //
-
+// Graph
 var graph;
 function myGraph() {
 
     // Add and remove elements on the graph object
     this.addNode = function (id) {
         nodes.push({"id": id});
-        update();
-    };
-
-    this.removeLink = function (source, target) {
-        for (var i = 0; i < links.length; i++) {
-            if (links[i].source.id == source && links[i].target.id == target) {
-                links.splice(i, 1);
-                break;
-            }
-        }
-        update();
+        update_network();
     };
 
     this.addLink = function (source, target, value) {
         links.push({"source": findNode(source), "target": findNode(target), "value": value});
-        update();
+        update_network();
     };
 
     var findNode = function (id) {
         for (var i in nodes) {
             if (nodes[i]["id"] === id) return nodes[i];
-        }
-        ;
+        };
     };
 
+    var findNodeIndex = function (id) {
+        for (var i = 0; i < nodes.length; i++) {
+            if (nodes[i].id == id) {
+                return i;
+            }
+        };
+    };
 
-    var vis = d3.select("#w2").append("svg")
-        .attr("width", w)
-        .attr("height", h)
+    this.resetLinks = function () {
+        links.splice(1, links.length);
+        update_network();
+    };
+
+    this.resetNodes = function () {
+        nodes.splice(2, nodes.length);
+        update_network();
+    };
 
     var force = d3.layout.force()
-        .gravity(.05)
-        .charge(-240*2)
-        .linkDistance(100)
-        .size([w, h]);
 
     var nodes = force.nodes();
     var links = force.links();
 
-    var update = function () {
+    var update_network = function () {
         var link = vis.selectAll("line")
                 .data(links, function (d) {
                     return d.source.id + "-" + d.target.id;
@@ -195,13 +202,11 @@ function myGraph() {
                     return d.source.id + "-" + d.target.id;
                 })
                 .attr("stroke-width", function (d) {
-                    return d.value / 10;
+                    return 5/Math.pow(nodes.length, 1.0/10);
                 })
                 .attr("class", "link");
+
         link.append("title")
-                .text(function (d) {
-                    return d.value;
-                });
         link.exit().remove();
 
         var node = vis.selectAll("g.node")
@@ -214,7 +219,13 @@ function myGraph() {
                 .call(force.drag);
 
         nodeEnter.append("svg:circle")
-                .attr("r", r)
+                .attr("r", function() {
+                    if (nodes.length > 2) {
+                        return r/Math.pow(nodes.length, 1.0/10)
+                    } else {
+                        return r
+                    }
+                })
                 .attr("id", function (d) {
                     return "Node;" + d.id;
                 })
@@ -224,51 +235,57 @@ function myGraph() {
                     d3.select("body").style("cursor", "default")
                 });
 
-        nodeEnter.append("svg:text")
-                .attr("class", "textClass")
-                .attr("x", -4.5)
-                .attr("y", 5)
-                .text(function (d) {
-                    return d.id;
-                })
-                .attr("font-family", "sans-serif")
-                .attr("font-size", "16px")
-                .attr("font-weight", "bold")
-                .attr("fill", "white");
+        // nodeEnter.append("svg:text")
+        //         .attr("class", "textClass")
+        //         .attr("x", -4.5)
+        //         .attr("y", 5)
+        //         .text(function (d) {
+        //             return d.id;
+        //         })
+        //         .attr("font-family", "sans-serif")
+        //         .attr("font-size", "16px")
+        //         .attr("font-weight", "bold")
+        //         .attr("fill", "white");
 
         node.exit().remove();
 
-        force.on("tick", function () {
-            node.attr("transform", function (d) {
-                return "translate(" + Math.max(r, Math.min(w - r, d.x)) + "," + Math.max(r, Math.min(h - r, d.y)) + ")";
-            });
-            link.attr("x1", function (d) { return d.source.x; })
-                .attr("y1", function (d) { return d.source.y; })
-                .attr("x2", function (d) { return d.target.x; })
-                .attr("y2", function (d) { return d.target.y; });
-        })
+        force
+            .on("tick", function () {
+                node.attr("transform", function (d) {
+                    return "translate(" + Math.max(r, Math.min(w - r, d.x)) + "," + Math.max(r, Math.min(h-50 - r, d.y)) + ")";
+                });
+                link.attr("x1", function (d) { return Math.max(r, Math.min(w - r, d.source.x)); })
+                    .attr("y1", function (d) { return Math.max(r, Math.min(h-50 - r, d.source.y)); })
+                    .attr("x2", function (d) { return Math.max(r, Math.min(w - r, d.target.x)); })
+                    .attr("y2", function (d) { return Math.max(r, Math.min(h-50 - r, d.target.y)); });
+            })
+            .gravity(.05*Math.pow(nodes.length, 1.0/4))
+            .charge(-240/Math.pow(nodes.length, 1.0/4))
+            .linkDistance(100/Math.pow(nodes.length, 1/1.1))
+            .size([w, h])
             .start()
 
     };
 
-
     // Make it all go
-    update();
+    update_network();
 }
 
 function drawGraph() {
+    graph = new myGraph("#w2");
 
-    graph = new myGraph("#svgdiv");
-
-    for (node=0; node<n_nodes; node++) {
-        graph.addNode(node);
-    }
+    graph.addNode(0);
+    graph.addNode(1);
+    graph.addLink(0, 1, 1);
 
     keepNodesOnTop();
-
 }
 
+
 drawGraph();
+drawHist();
+drawControls();
+
 
 // because of the way the network is created, nodes are created first, and links second,
 // so the lines were on top of the nodes, this just reorders the DOM to put the svg:g on top
@@ -278,8 +295,60 @@ function keepNodesOnTop() {
         gnode.parentNode.appendChild(gnode);
     });
 }
-function addNodes() {
-    d3.select("svg")
-            .remove();
-     drawGraph();
+
+window.addRandomNode = function() {
+    var new_id = d3.keys(N).length
+    var conn_id = D.randomElement()
+    graph.addNode(new_id);
+    graph.addLink(new_id, conn_id);
+    keepNodesOnTop();
+    D.push(new_id, conn_id);
+    N[new_id] = [conn_id]
+    N[conn_id].push(new_id)
+    update_histogram()
+}
+
+window.playProcess = function() {
+    var step = -1;
+    function nextval() {
+        step++;
+        return (100*step); // initial time, wait time
+    }
+    for (i=0; i<10; i++){
+        setTimeout(function() {
+            addRandomNode()
+        }, nextval());
+    }
+}
+
+window.restart = function(){
+    graph.resetNodes()
+    graph.resetLinks()
+    D = [0, 1]
+    N = {0: [1], 1: [0]}
+    window.clear_bars()
+    window.update_histogram()
+}
+
+Array.prototype.randomElement = function () {
+    return this[Math.floor(Math.random() * this.length)]
+}
+
+Array.prototype.getUnique = function(){
+    var uniques = [];
+    for(var i = 0, l = this.length; i < l; ++i){
+        if(this.lastIndexOf(this[i]) == this.indexOf(this[i])) {
+            uniques.push(this[i]);
+        }
+    }
+    return uniques;
+}
+
+Array.prototype.shuffle = function() {
+  let m = this.length, i;
+  while (m) {
+    i = (Math.random() * m--) >>> 0;
+    [this[m], this[i]] = [this[i], this[m]]
+  }
+  return this;
 }
