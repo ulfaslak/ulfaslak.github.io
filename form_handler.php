941dd2022d3ea553fc092ffedc5d3516f662106f
ulfaslak <ulfjensen@gmail.com>
2017-10-07 13:46:51 +0200

    .

--- form_handler.php
+++ form_handler.php
@@ -2,11 +2,11 @@
 $to      = 'ulfjensen@gmail.com';
 $subject = 'Computational Analysis of Big Data feedback';
 $message = $_POST['textarea'];
-$headers = 'From: webmaster@qixty.com' . "\r\n" .
-'Reply-To: contact@qixty.com' . "\r\n" .
-'X-Mailer: PHP/' . phpversion();
+// $headers = 'From: webmaster@qixty.com' . "\r\n" .
+// 'Reply-To: contact@qixty.com' . "\r\n" .
+// 'X-Mailer: PHP/' . phpversion();
 
-$send = mail($to, $subject, $message, $headers);
+$send = mail($to, $subject, $message);//, $headers);
 
 // this will help you to get the status mail sent or not
 if($send) :
