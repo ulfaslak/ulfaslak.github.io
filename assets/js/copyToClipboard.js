ba2ab0003e9d8fa590bbd2915acbdf527d249c43
ulfaslak <ulfjensen@gmail.com>
2020-07-15 09:42:43 +0200

    add freelance page

--- assets/js/copyToClipboard.js
+++ assets/js/copyToClipboard.js
@@ -1,24 +1,24 @@
-function copyToClipboard() {
+function copyToClipboard(value, type) {
 	var input = document.createElement('input');
-	input.setAttribute('value', "ulfaslak@gmail.com");
+	input.setAttribute('value', value);
 	document.body.appendChild(input);
 	input.select();
 	document.execCommand('copy');
 	document.body.removeChild(input)
 	swal({
-	  	text: "Email copied to clipboard!",
+	  	text: type + " copied to clipboard!",
 	  	timer: 1500,
 	  	class: "swal-modal",
 	  	buttons: false
 	});
 }
 
-function copyToClipboardMobile() {
+function copyToClipboardMobile(value, type) {
 	var input = document.createElement('input');
-	input.setAttribute('value', "ulfaslak@gmail.com");
+	input.setAttribute('value', value);
 	document.body.appendChild(input);
 	input.select();
 	document.execCommand('copy');
 	document.body.removeChild(input)
-	alert("Email copied to clipboard!")
+	alert(type + " copied to clipboard!")
 }
